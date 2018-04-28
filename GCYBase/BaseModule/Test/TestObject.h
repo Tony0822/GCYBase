b60dbdb2fc1b78680bea74f9261eff5dffc30b96
gaochongyang <gaochongyang@enmonster.com>
2018-04-28 15:34:42 +0800

    测试

---
+++ GCYBase/BaseModule/Test/TestObject.h
@@ -0,0 +1,12 @@
+//
+//  TestObject.h
+//  GCYBase
+//
+//  Created by gaochongyang on 2018/4/28.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObject : NSObject
+
+@end
---
+++ GCYBase/BaseModule/Test/TestObject.m
@@ -0,0 +1,12 @@
+//
+//  TestObject.m
+//  GCYBase
+//
+//  Created by gaochongyang on 2018/4/28.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+@end
